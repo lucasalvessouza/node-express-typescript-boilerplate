3170837e0331c7df70af10e1a4a61f7d39909b65
Lucas Alves <lucasalves.souza01@gmail.com>
2022-06-15 20:20:31 -0300

    chore: refactoring eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,24 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
+  root: true,
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: [
-    '@typescript-eslint',
+  plugins: ['@typescript-eslint', 'eslint-plugin-import-helpers'],
+  overrides: [
+    {
+      files: ['*.ts'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        'no-console': 'warn',
+        'import-helpers/order-imports': [
+          'warn',
+          {
+            newlinesBetween: 'never',
+            groups: ['module', ['parent', 'sibling', 'index']],
+            alphabetize: { order: 'asc', ignoreCase: true },
+          },
+        ],
+      },
+    },
   ],
-  rules: {
-    semi: ['error', 'never'],
-    'class-methods-use-this': 'off',    
-  },
 }
